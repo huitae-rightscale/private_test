2472b4025f534e84e474af1154e0f7d151b2cf40
huitae-rightscale <huitae.kim@rightscale.com>
2013-10-18 18:15:51 +0900

    commit for install php

--- cookbooks/hello/recipes/default.rb
+++ cookbooks/hello/recipes/default.rb
@@ -1,9 +1,16 @@
-
 rightscale_marker
 
-log node[:hello][:messages]
+cookbook_file "/etc/yum.repo.d/rpmforge.repo" do
+  source "rpmforge.repo"
+  owner "root"
+  group "root"
+  mode 0644
+end
 
-log "Installing #{node[:packages]}"
+log "Installing php"
 
+package "php" do
+    action :install
+end
 
 
