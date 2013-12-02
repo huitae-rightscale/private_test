4e756e64f87999b821b14201eed42fc417ee1c03
Huitae Kim <huitae.kim@rightscale.com>
2013-12-02 10:47:58 +0900

    use bash block

--- cookbooks/nodejs/definitions/default.rb
+++ cookbooks/nodejs/definitions/default.rb
@@ -2,15 +2,15 @@ define :nodejs_module, :enable => true do
 
     log "installing nodejs module : #{params[:name]}"
 
-    execute "install npm module" do
-      command "npm -g install #{params[:name]}"
+#    execute "install npm module" do
+#      command "npm -g install #{params[:name]}"
+#    end
+
+    bash "install_npm_module" do
+      code <<-EOM
+        npm -g install #{params[:mod]}
+      EOM
     end
 
-#    bash "install_npm_module" do
-#      code <<-EOM
-#        npm install #{params[:mod]}
-#        mkdir /usr/local/jboss
-#      EOM
-#    end
     log "installation is done : #{params[:name]}"
 end
