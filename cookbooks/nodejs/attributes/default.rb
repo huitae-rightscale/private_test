a015dea499720a6296ca2e3223f9f8c2b5acf480
Huitae Kim <huitae.kim@rightscale.com>
2013-12-03 14:59:32 +0900

    add support for CentOS

--- cookbooks/nodejs/attributes/default.rb
+++ cookbooks/nodejs/attributes/default.rb
@@ -6,8 +6,10 @@
 # http://www.rightscale.com/terms.php and, if applicable, other agreements
 # such as a RightScale Master Subscription Agreement.
 
-default[:nodejs][:version] = "0.10.22"
+default[:nodejs][:version]      = "0.10.22"
+default[:nodejs][:bin_baseurl]  = "http://nodejs.org/dist/v#{node[:nodejs][:version]}"
+default[:nodejs][:tar]          = "node-v#{node[:nodejs][:version]}-linux-x64.tar.gz"
+default[:nodejs][:source]       = "http://nodejs.org/dist/v#{node[:nodejs][:version]}/#{node[:nodejs][:tar]}"
+default[:nodejs][:bin_path]     = "/usr/local/nodejs"
 default[:nodejs][:checksum_linux_x64] = "3739f75bbb85c920a237ceb1c34cb872409d61f7"
-default[:nodejs][:module_list] = []
-default[:nodejs][:module_global] = "false"
-
+default[:nodejs][:module_list]  = []
--- cookbooks/nodejs/recipes/install_server.rb
+++ cookbooks/nodejs/recipes/install_server.rb
@@ -8,6 +8,11 @@
 
 rightscale_marker
 
+user "nodejs" do
+  home "/usr/local/nodejs"
+  group "nodejs"
+end
+
 case node["platform"]
   when "ubuntu"
     repo = "http://ppa.launchpad.net/chris-lea/node.js/ubuntu"
@@ -20,14 +25,41 @@ case node["platform"]
       key "C7917B12"
       action :add
     end
+
+    packages.each do |p|
+      package p
+    end
+
   when "centos"
-    packages = ["nodejs", "nodejs-devel", "npm"]
+    remote_file "/tmp/#{node[:nodejs][:tar]}" do
+      owner "root"
+      group "root"
+      mode "0644"
+      source "#{node[:nodejs][:source]}"
+      checksum "#{node[:nodejs][:checksum_linux_x64]}"
+    end
+
+    bash "unpack_nodejs" do
+      cwd "/tmp"
+      code <<-EOM
+        tar zxf #{node[:nodejs][:tar]} --strip=1 -C /usr/local/nodejs
+        chown -R nodejs:nodejs /usr/local/nodejs
+        chmod 0755 /usr/local/nodejs
+      EOM
+      not_if { ::File.exists?("/usr/local/nodejs/bin") }
+    end
+
+    template "/etc/profile.d/nodejs.sh" do
+        source "nodejs.sh.erb"
+        owner "root"
+        group "root"
+        cookbook "nodejs"
+        variables({
+            :nodejs_install_path    => "#{[:nodejs][:bin_path]"
+        })
+    end
   else
     log "There are no nodejs packages for this platform; please use the source or binary method to install node"
-end
-
-packages.each do |p|
-  package p
 end
 
 log "Node.js is installed successfully!!"
