a5c3394a4afd5be3b3f154d7258470c7edb4f99e
Huitae Kim <huitae.kim@rightscale.com>
2013-12-09 22:55:31 +0900

    add resource attribute

--- cookbooks/app_nodejs/resources/default.rb
+++ cookbooks/app_nodejs/resources/default.rb
@@ -1,3 +1,8 @@
 # Install Node.js modules
 actions :install_modules
 
+    # name of modules
+    attribute :mode_name, :kind_of => String
+    # global switch
+    attribute :g_option, :kind_of => String
+
