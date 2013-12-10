43ddc621a261447b2fd67edcaf9a71a36e24d0dd
Huitae Kim <huitae.kim@rightscale.com>
2013-12-10 15:13:13 +0900

    fix typo

--- cookbooks/app_nodejs/definitions/app_nodejs.rb
+++ cookbooks/app_nodejs/definitions/app_nodejs.rb
@@ -14,8 +14,8 @@ define :app_nodejs, :options => "", :mod_name => nil, :path => nil, :g_option =>
   log "Create path to install in #{params[:path]} if not exists...."
 
   directory "#{params[:path]}" do
-    ower nodejs
-    group nodejs
+    ower "root"
+    group "root"
     mode 0755 
     recursive true
     action :create
