c7a8161f2157573ccc66a6c249131fa1bf04558c
Huitae Kim <huitae.kim@rightscale.com>
2013-12-10 15:01:31 +0900

    fix typo

--- cookbooks/app_nodejs/attributes/default.rb
+++ cookbooks/app_nodejs/attributes/default.rb
@@ -7,9 +7,9 @@
 # such as a RightScale Master Subscription Agreement.
 
 default[:app_nodejs][:version]      = "0.10.22"
-default[:app_nodejs][:bin_baseurl]  = "http://nodejs.org/dist/v#{node[:nodejs][:version]}"
-default[:app_nodejs][:tar]          = "node-v#{node[:nodejs][:version]}-linux-x64.tar.gz"
-default[:app_nodejs][:source]       = "http://nodejs.org/dist/v#{node[:nodejs][:version]}/#{node[:nodejs][:tar]}"
+default[:app_nodejs][:bin_baseurl]  = "http://nodejs.org/dist/v#{node[:app_nodejs][:version]}"
+default[:app_nodejs][:tar]          = "node-v#{node[:app_nodejs][:version]}-linux-x64.tar.gz"
+default[:app_nodejs][:source]       = "http://nodejs.org/dist/v#{node[:app_nodejs][:version]}/#{node[:app_nodejs][:tar]}"
 default[:app_nodejs][:bin_path]     = "/usr/local/nodejs"
 default[:app_nodejs][:checksum_linux_x64] = "3739f75bbb85c920a237ceb1c34cb872409d61f7"
 default[:app_nodejs][:module_list]  = []
